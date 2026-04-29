dbf38e59571f9f2bd87ec98094284af2d4207ffe
Vaibhav singh <vaibhavpalsingh1234@gmail.com>
2026-04-29 15:24:35 +0530

    updated api url for deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://playto-b8aa.onrender.com",
 });
 
 export const getBalance = (id) =>
